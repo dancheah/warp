909bc0cf89af80149a54cf2ba068c505e74edb51
Daniel Cheah <me@dancheah.com>
2011-04-06 21:04:02 -0700

    Got pong to run. Not quite sure about the differences between ByteString, Text, String, IsString and OverloadedStrings. Probably worth a side diversion.

--- pong.hs
+++ pong.hs
@@ -1,21 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
 import Network.Wai.Handler.Warp
-import Blaze.ByteString.Builder (copyByteString)
+import Blaze.ByteString.Builder
 import Data.Monoid
 import Data.Enumerator (run_, enumList, ($$))
+import Network.HTTP.Types
+import Blaze.ByteString.Builder.Char.Utf8
+import qualified Data.ByteString as B
 
 main = run 3000 app
 
-app req = return $
-    case pathInfo req of
-        "/builder/withlen" -> builderWithLen
-        "/builder/nolen" -> builderNoLen
-        "/enum/withlen" -> enumWithLen
-        "/enum/nolen" -> enumNoLen
-        "/file/withlen" -> fileWithLen
-        "/file/nolen" -> fileNoLen
-        _ -> index $ pathInfo req
+app1 req = return $ 
+            case rawPathInfo req of 
+                "/file/nolen" -> fileNoLen
+                _ -> index $ rawPathInfo req
+
+app req = return $ 
+   case rawPathInfo req of
+       "/builder/withlen" -> builderWithLen
+       "/builder/nolen" -> builderNoLen
+       "/enum/withlen" -> enumWithLen
+       "/enum/nolen" -> enumNoLen
+       "/file/withlen" -> fileWithLen
+       "/file/nolen" -> fileNoLen
+       _ -> index $ rawPathInfo req
 
 builderWithLen = ResponseBuilder
     status200
@@ -35,13 +43,13 @@ fileWithLen = ResponseFile
     [ ("Content-Type", "text/plain")
     , ("Content-Length", "4")
     ]
-    "pong.txt"
+    "pong.txt" Nothing
 
 fileNoLen = ResponseFile
     status200
     [ ("Content-Type", "text/plain")
     ]
-    "pong.txt"
+    "pong.txt" Nothing
 
 enumWithLen = ResponseEnumerator $ \f ->
     run_ $ (enumList 1 $ map copyByteString ["P", "O", "NG"]) $$ f
@@ -56,6 +64,7 @@ enumNoLen = ResponseEnumerator $ \f ->
         [ ("Content-Type", "text/plain")
         ]
 
+index :: B.ByteString -> Response
 index p = ResponseBuilder status200 [("Content-Type", "text/html")] $ mconcat $ map copyByteString
     [ "<p><a href='/builder/withlen'>builder withlen</a></p>\n"
     , "<p><a href='/builder/nolen'>builder nolen</a></p>\n"
